3420e7abedb66a6b01b873769e63904063c75d6f
iftakhar <iftakharalamshuvo11@gmail.com>
2024-03-15 22:44:14 +0600

    our recipes some end

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Navigation from './components/Navigation/Navigation'
+import OurRecepies from './components/OurRecepies/OurRecepies'
 
 function App() {
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <>
     <nav className='container mx-auto'>
     <Navigation></Navigation>
     </nav>
-    <main>
-      <section className='container mx-auto p-3 mt-10 lexend'>
+    <main className='container mx-auto'>
+      <section className=' p-3 mt-10 lexend'>
         <Banner></Banner>
+      </section>
+      <section className=' p-3 mt-10 lexend'>
+        <OurRecepies></OurRecepies>
       </section>
     </main>
       
---
+++ src/components/OurRecepies/OurRecepies.jsx
@@ -0,0 +1,14 @@
+
+
+const OurRecepies = () => {
+    return (
+        <div>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-[#150B2B] text-2xl font-bold mb-5">Our Recipes</h1>
+                <p className="text-center md:w-[700px] font-normal  text-[#150b2b99] md:leading-9">Our recipes primarily use fresh, unprocessed ingredients but we also believe there is a time and a place for canned, frozen, and other prepared ingredients. We believe in a diet that includes a wide variety of foods:</p>
+            </div>
+        </div>
+    );
+};
+
+export default OurRecepies;
