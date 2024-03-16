f4d0511145786349a86e873f2e967e094a7b9ad5
iftakhar <iftakharalamshuvo11@gmail.com>
2024-03-16 17:01:30 +0600

    some fixes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -16,12 +16,17 @@ const Navigation = () => {
 
           <div className=" gap-2">
         
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="Search"
-                className="input input-bordered w-24 md:w-auto md:rounded-full"
-              />
+            <div className="form-control flex">
+             
+            
+            <label className="input input-bordered w-24 md:w-auto md:rounded-full flex items-center ">
+  <input type="text" className=" input-bordered w-24"  placeholder="Search" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+</label>
+              
+
+            
+               
             </div>
             <div>
               <div
